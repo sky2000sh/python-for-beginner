e17b56c1ec4b1eeeb252019cc62e6bcdfe8d8b04
sky2000sh <sky2000sh@gmail.com>
2022-06-18 23:47:54 +0900

    Extracting Indeed Pages on 18/06/22

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+# print(indeed_result.text)
 
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+# print(indeed_soup)
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+# print(pagination)
+
+pages = pagination.find_all('a')
+# print(pages)
+
+spans = []
+for page in pages :
+    spans.append(page.find("span"))
+    
+# print(spans[0:-1])
+print(spans[:-1])
